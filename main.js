b74616ee1f5026df36c4ac91124b5b099a294b24
Esfahani <esfahanimichael@gmail.com>
2018-10-23 17:51:22 -0700

    more updates to wiki api

--- main.js
+++ main.js
@@ -57,13 +57,11 @@ function settings(parameter, short_name){
         },
         success: function( response ){
             var data = Object.values(response.query.pages)[0].extract;
-            if (short_name === "ale2" || short_name === "stout2" || short_name === "lager2" ||  short_name ==="malt2" || short_name === "non-alcohol2") {
+            if (short_name === "Ale2" || short_name === "Stout2" || short_name === "Lager2" ||  short_name ==="Malt2" || short_name === "Non-Alcoholic Beer2") {
                 typesBeer[short_name] = data;
 
             }
             else{
-                console.log('HIIIIIIIIIII!!!!!!')
-
                 renderingDescriptionOnDom(data, short_name);
             }
 
@@ -72,35 +70,31 @@ function settings(parameter, short_name){
     $.ajax(ajaxConfig);
 }
 function callingStoreTypesOfBeer(){ ///////I HAVE STORED ALL 5 TYPES OF BEERS IN AN OBJECT SO WE CAN USE THEM FOR ERROR HANDLING
-    settings("ale beer", "ale2");
-    settings("stout beer", "stout2");
-    settings("lager beer", "lager2");
-    settings("malt beer", "malt2");
-    settings("Non-alcoholic drink", "non-alcohol2");
+    settings("ale beer", "Ale2");
+    settings("stout beer", "Stout2");
+    settings("lager beer", "Lager2");
+    settings("malt beer", "Malt2");
+    settings("Non-alcoholic drink", "Non-Alcoholic Beer2");
 }
 
 callingStoreTypesOfBeer(); ////GETTING THOSE BEERS IN THAT ARRAY FOR ERROR HANDLING
-
-console.log('hi', typesBeer);
-
+console.log(typesBeer);
 function findingDescription(parameterRender1, parameterRender2) ///PARAM TAKES NAME OF BEER AND PASSES TO THE SETTINGS FUNCTION
 {
     settings(parameterRender1, parameterRender2);
 }
 function renderingDescriptionOnDom(param1, param2){ ///PARAM1 IS THE DESCRIPTION(FROM WIKI) WE GOT FROM SETTINGS FUNCTION, PARAM2 IS ONE OF 5 BEERS, DEFAULT IF NO DESCRIPTION FROM WIKIPEDIA
-    if (param1.length > 100){
-        $('.wikipedia').text(param1);
-    }
-    else{
-        param2 = `${param2}2`; ///HAD TO DO THIS BECAUSE param2 ONLY EQUALS ex: ale AND WE NEED IT TO HAVE A 2 AT THE END TO USE IT AS A KEY VALUE PAIR FROM TYPES BEER OBJ
+
+    if (!param1){ ///IF PARAM1 IS UNDEFINED (IT WOULD BE UNDEFINED IF THERE IS NO PAGE OR IF THE PAGE DOES NOT GET LOADED WHEN WE DO THE API CALL
+       param2 = `${param2}2`; ///HAD TO DO THIS BECAUSE param2 ONLY EQUALS ex: ale AND WE NEED IT TO HAVE A 2 AT THE END TO USE IT AS A KEY VALUE PAIR FROM TYPES BEER OBJ
         var errorRenderingFromTypesBeer = typesBeer[param2];
-        $('.wikipedia').text(errorRenderingFromTypesBeer);
-    }
-
-
+       $('.wikipedia').text(errorRenderingFromTypesBeer);
+   }
+   else{
+       $('.wikipedia').text(param1);
+   }
 
 }
-var practiceBeer = "Blue Moon"
-console.log(`${practiceBeer} beer`);
+var practiceBeer = "sol"
 
-findingDescription(`${practiceBeer} beer`, "ale") //// CALLING FUNCTION FOR PRACTICE
+findingDescription(`${practiceBeer} beer`, "Lager") //// CALLING FUNCTION FOR PRACTICE
